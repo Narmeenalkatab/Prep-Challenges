ccf662df2f7b7a0be006cb0ca5e9e08df3c8610f
Narmeen alkatab <narmeen.kattab1@gmail.com>
2023-04-03 19:39:41 +0000

    all solved

--- code-challenges/challenges-09.js
+++ code-challenges/challenges-09.js
@@ -11,12 +11,12 @@
 // Required:
 
 //  Write a function that takes an array of numbers and increase the values by 10
-//  
+//
 //  Input:
 //  [20, 54, 89, 41]
 //  Output:
 //  [30, 64, 99, 51]
-//  
+//
 
 const arrInc = (arr) => {
     let result = [];
@@ -29,21 +29,21 @@ arr.forEach(element  => {result.push(element+10)});
 
 // -------------------------------------------------------------------------------------------------------
 // Challenge 02:
-// optional: 
+// optional:
 
-//  Write a function that takes an array of decimals and round every value to the closest value 
-//  
+//  Write a function that takes an array of decimals and round every value to the closest value
+//
 //  Input:
-//  [5.4, 5.5 ,6.7, 6.8] 
+//  [5.4, 5.5 ,6.7, 6.8]
 //  Output:
 //  [5, 6, 7, 7]
-// 
+//
 const roundDecimals = (arr) => {
     // write your code here
     let value=[];
     for(let i=0;i<arr.length;i++){
        value.push(Math.round(arr[i]));
-        
+
     }
     return value;
 }
@@ -52,10 +52,10 @@ const roundDecimals = (arr) => {
 // -------------------------------------------------------------------------------------------------------
 // Challenge 03:
 // optional:
-// 
+//
 // An owner of a factory wants to give a 100$ bonus for production department employees who worked *More than* 8 hours
-// and 50$ for those who worked less, given their data increase their salary and return the data back again 
-//  
+// and 50$ for those who worked less, given their data increase their salary and return the data back again
+
 // Input:
 // let data = [
 //     {
@@ -83,7 +83,7 @@ const roundDecimals = (arr) => {
 //         salary: "3200$"
 //     }
 // ]
-// 
+
 // Output:
 // [
 //     {
@@ -115,19 +115,19 @@ const roundDecimals = (arr) => {
 
 const employeesBonus = (arr) => {
     // write your code here
+let array1= [];
+arr.forEach(i =>{
+ if (i.workHours > 8) {
+   let result = parseInt(i.salary) + 100;
+   i.salary = result + "$";
+ } else {
+   let result = parseInt(i.salary) + 50;
+   i.salary = result + "$";
+ }
+ array1.push(i);
 
-for(let i=0;i<arr.length;i++){
-if(arr[i].workHours<= 8){
-    arr[i].salary=(arr[i].salary+50);
-   
-}
-else if(arr[i].workHours> 8){
-    arr[i].salary=(arr[i].salary+100);
-    
-}
-
-}
-return arr;
+});
+return array1;
 }
 // -------------------------------------------------------------------------------------------------------
 
@@ -138,12 +138,12 @@ return arr;
 // Harry wants to buy a new mouse and keyboard for his new setup
 // He wants to choose one mouse and one keyboard from the list of prices and take the most expensive combination
 // but his budget is limited, help him by finding the most expensive *price* for a combination to buy with his budget
-// 
+//
 // Input:
 // budget = 200$
 // mouseArray = [35, 15, 75, 180, 150, 50]
 // keyBoardArray = [5, 150, 35, 120, 75, 50, 100]
-// 
+//
 // Output: 200
 
 const mostExpensive = (budget, mouseArray, keyBoardArray) => {
@@ -154,7 +154,7 @@ const mostExpensive = (budget, mouseArray, keyBoardArray) => {
     for(let i=0;i<mouseArray.length;i++){
 
   maxM =Math.max(mouseArray[i]);
-           
+
         }
     for(let i=0;i<keyBoardArray.length;i++){
         maxK =Math.max(keyBoardArray[i]);
